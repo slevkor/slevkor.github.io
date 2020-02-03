bdc0cd8ee8b13e68fa1c2525f128d0933ca718c6
LevKordonsky <l.kordonsky@f5.com>
2020-02-03 22:16:34 +0200

    Updates

---
+++ precache-manifest.429bac902e7141ad74ec677ddf13ddfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48adb423472f69fda550d8f9641ed527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e83f851b19e4358f9d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3882e50b92521e3d9733",
+    "url": "/static/js/2.9c1239dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9c1239dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e83f851b19e4358f9d",
+    "url": "/static/js/main.38694448.chunk.js"
+  },
+  {
+    "revision": "84e3cfd4c70bb7ba99bf",
+    "url": "/static/js/runtime-main.5729d020.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
