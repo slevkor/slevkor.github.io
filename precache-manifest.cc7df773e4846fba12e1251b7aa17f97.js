0759c265096371b202722ef22eb3e34dbadfbbd3
LevKordonsky <l.kordonsky@f5.com>
2020-02-03 21:55:01 +0200

    Updates

---
+++ precache-manifest.cc7df773e4846fba12e1251b7aa17f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649e8f6bc4b29f2ddcb4c22fc17a946a",
+    "url": "/lev-react-app/index.html"
+  },
+  {
+    "revision": "9eec0af43e699ed6d45f",
+    "url": "/lev-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3882e50b92521e3d9733",
+    "url": "/lev-react-app/static/js/2.9c1239dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lev-react-app/static/js/2.9c1239dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eec0af43e699ed6d45f",
+    "url": "/lev-react-app/static/js/main.5573bdb0.chunk.js"
+  },
+  {
+    "revision": "6b9b3303cc3f5bc75c05",
+    "url": "/lev-react-app/static/js/runtime-main.382faba7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lev-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
